3a885b455dbf58b996ba696953a973b819950c26
Patrick O'Hara <patrick.o@clubware.co.nz>
2019-12-11 09:37:33 +1300

    get stock data for calculations on which stock to pick

--- scraper.py
+++ scraper.py
@@ -2,18 +2,48 @@ from bs4 import BeautifulSoup
 import requests
 import json 
 
-url = 'http://web.mta.info/developers/turnstile.html'
+url = 'https://finance.yahoo.com/gainers'
 response = requests.get(url,timeout=5)
 soup = BeautifulSoup(response.text,"html.parser")
 
 data={}
-data['links']=[]
+data['stocks']=[]
 
-for i in range(36,len(soup.findAll('a'))+1): #'a' tags are for links
-    one_a_tag = soup.findAll('a')[i-1]
-    link = one_a_tag['href']
-    download_url = 'http://web.mta.info/developers/'+ link
-    data['links'].append(download_url)
+class Stock:
+    def __init__(self,symbol,name, price,dollar_change,percent_change,volume):
+        self.name = name
+        self.price = price
+        self.symbol = symbol
+        self.dollar_change = dollar_change
+        self.percent_change = percent_change
+        self.volume = volume
 
-with open('data.txt','w') as outfile:
-    json.dump(data,outfile,indent=2,sort_keys=True)
+    def __repr__(self):
+        return { 
+            'symbol':self.symbol, 
+            'name': self.name, 
+            'price': self.price, 
+            'dollar_change': self.dollar_change,
+            'percent_change': self.percent_change,
+            'volume': self.volume 
+        }
+
+
+table_rows = soup.find_all('tr',{'class':'simpTblRow'})
+
+for row in table_rows:
+    symbol = row.find('td', {'aria-label': 'Symbol'}).a.text
+    name = row.find('td', {'aria-label': 'Name'}).text
+    price = row.find('td',{'aria-label':'Price (Intraday)'}).span.text
+    dollar_change = row.find('td', {'aria-label': 'Change'}).span.text
+    percent_change = row.find('td', {'aria-label': '% Change'}).span.text
+    volume = row.find('td', {'aria-label': 'Volume'}).span.text
+    # Create new object
+    stock_data = Stock(symbol,name,price,dollar_change,percent_change,volume)
+    # Append to data object
+    data['stocks'].append(stock_data.__repr__())
+
+
+with open('stock-data.json', 'w') as f:
+    for stock in data['stocks']:
+        json.dump(stock,f,indent=2,sort_keys=True)
