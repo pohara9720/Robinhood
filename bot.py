9a39eeca2f7808ce4252eef068d0087cb9514466
Patrick O'Hara <patrick.o@clubware.co.nz>
2019-12-12 08:41:27 +1300

    simplify file to only run when picking stocks

--- bot.py
+++ bot.py
@@ -1,16 +1,22 @@
 import json
 from select_stock import select_stock
 from scraper import scrape_stocks
-## Scrape the web for stocks
-scrape_stocks()
+
 
 todays_stocks = []
 
-with open('stock_data.json') as json_file:
-    data = json.load(json_file)
-    stock_data = data['stocks']
-    selected_stocks = select_stock(stock_data)
-    for stock in selected_stocks:
-        todays_stocks.append(stock)
+##Generate array of stocks that meet critieria to trade today
+def pick_stocks_for_today():
+    ## Scrape the web for stocks
+    scrape_stocks()
 
-print('Todays stocks chosen are: ', todays_stocks)
+    with open('stock_data.json') as json_file:
+        data = json.load(json_file)
+        stock_data = data['stocks']
+        selected_stocks = select_stock(stock_data)
+        for stock in selected_stocks:
+            todays_stocks.append(stock)
+
+        print('Todays stocks chosen are: ', todays_stocks)
+
+pick_stocks_for_today()
