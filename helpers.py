6b1345be594a577b3b3c1f1d133e56ddc0a97ee8
Patrick O'Hara <patrick.o@clubware.co.nz>
2019-12-12 08:45:02 +1300

    use consistent var names

--- helpers.py
+++ helpers.py
@@ -8,9 +8,9 @@ def convert_price(price_string):
 # Convert volume to workable integer
 def convert_volume(volume_string):
     millions = volume_string.count('M') 
-    remove_m = volume_string.replace('M','')
-    remove_commas = remove_m.replace(',','')
-    volume_float = float(remove_commas)
+    no_m = volume_string.replace('M','')
+    no_commas = no_m.replace(',','')
+    volume_float = float(no_commas)
     if millions >= 1:
         return int(volume_float * 1000000)
     else:
