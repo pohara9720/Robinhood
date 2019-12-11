4f7005ee6b4e980cca3fe74c9877169f1c342e3c
Patrick O'Hara <patrick.o@clubware.co.nz>
2019-12-12 08:43:28 +1300

    move empty array inside function and return so it can be stored in a variable

--- bot.py
+++ bot.py
@@ -3,10 +3,12 @@ from select_stock import select_stock
 from scraper import scrape_stocks
 
 
-todays_stocks = []
+
 
 ##Generate array of stocks that meet critieria to trade today
 def pick_stocks_for_today():
+    ## Array of stocks to populate
+    todays_stocks = []
     ## Scrape the web for stocks
     scrape_stocks()
 
@@ -16,7 +18,10 @@ def pick_stocks_for_today():
         selected_stocks = select_stock(stock_data)
         for stock in selected_stocks:
             todays_stocks.append(stock)
+        return todays_stocks
 
-        print('Todays stocks chosen are: ', todays_stocks)
+        
 
-pick_stocks_for_today()
+stocks = pick_stocks_for_today()
+
+print('Todays stocks chosen are: ', stocks)
